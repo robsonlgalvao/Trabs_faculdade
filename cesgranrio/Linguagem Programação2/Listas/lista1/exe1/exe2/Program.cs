57f0c95abd9b779cd6246cb10d7d371f8059932a
Robson Galvão <68071194+robsonlgalvao@users.noreply.github.com>
2023-09-14 22:33:34 -0300

    14-09-2023

--- cesgranrio/Linguagem Programação2/Listas/lista1/exe1/exe2/Program.cs
+++ cesgranrio/Linguagem Programação2/Listas/lista1/exe1/exe2/Program.cs
@@ -5,11 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace exe2
-{
+{//            Desenvolva um programa para ler o valor do raio de um círculo, e depois mostrar o
+ //            valor da área deste círculo com quatro casas decimais.
+ //            a.Fórmula da área: area = π.raio2
+ //            b.Considere o valor de π = 3.14159
     internal class Program
     {
         static void Main(string[] args)
         {
+            double raio = 0 ,area = 0,pi = 3.14159;
+            
+            Console.WriteLine("Digite o Raio do círculo: ");
+            raio = float.Parse(Console.ReadLine());
+
+            area = pi * Math.Pow(raio, 2);
+            Console.WriteLine($"A área desse círculo é: {area.ToString("F4")}");
+
+            Console.ReadKey();
         }
     }
 }
