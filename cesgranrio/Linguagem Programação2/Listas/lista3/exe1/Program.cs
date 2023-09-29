33d70c5f775ca578dba29df117642106129a4229
Robson Galvão <68071194+robsonlgalvao@users.noreply.github.com>
2023-09-29 19:56:22 -0300

    c#lista3

---
+++ cesgranrio/Linguagem Programação2/Listas/lista3/exe1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Desenvolva um programa no qual o usuário entre com dois números quaisquer. 
+          Em seguida será exibido na tela as opções de operação: Adição, Subtração, Multiplicação e Divisão. 
+          Após a seleção da operação o programa exibirá a operação escolhida e o resultado.
+          */
+            int num1 = 0, num2 = 0;
+            char op;
+            //entrada do usuário
+            Console.Write("Digite um número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Agora digite a operação:ex +,-,*,/: ");
+            op = char.Parse(Console.ReadLine());
+
+            Console.Clear();
+            //saida de acordo com a escolha
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine($"Você escolheu a soma veja o resultado:\n{num1} + {num2} = {num1+num2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Você escolheu a subtração veja o resultado:\n{num1} - {num2} = {num1-num2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Você escolheu a multiplicação veja o resultado:\n{num1} * {num2} = {num1*num2}");
+                    break;
+                case '/':
+                    Console.WriteLine($"Você escolheu a divisão veja o resultado:\n{num1} / {num2} = {num1/num2}");
+                    break;  
+                default: 
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cesgranrio/Linguagem Programação2/Listas/lista3/exe2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe2
+{
+    internal class Program
+    {
+        struct func{
+            public string nome;
+            public float salario;
+            public int filhos,temptrab;            
+
+        }
+        static void Main(string[] args)
+        {/*Desenvolva um programa no qual o usuário entrará com os seguintes dados:
+            a. Nome do Funcionário;
+            b. Salário;
+            c. Número de filhos;
+            d. Tempo de trabalho (Anos)
+            *. O programa deverá fazer o seguinte processamento:
+                1. Exibir o nome do funcionário;
+                2. Exibir salário atual;
+                3. Exibir Número de filhos.
+                4. Exibir salário novo >> Se o tempo de trabalho na empresa for entre 3 e 5 anos. 
+                    O salário será aumentado em 5%; 
+            Se o tempo de trabalho na empresa for entre 5 e 9 anos. 
+                    O salário será aumentado em 10%. 
+            Se o tempo de trabalho na empresa for maior que 10 anos nos. 
+                    O salário será aumentado em 20%.
+                5. Acrescentar R$ 500,00 por filho. Com limite máximo de 3 filhos.
+          */
+
+            func empresa;
+
+            Console.WriteLine("Digite o nome do Funcionário: ");
+            empresa.nome = Console.ReadLine();
+            Console.WriteLine($"Digite o salário de {empresa.nome}");
+            empresa.salario = float.Parse( Console.ReadLine() );
+            Console.WriteLine($"{empresa.nome} tem quantos filhos?");
+            empresa.filhos = int.Parse(Console.ReadLine() );
+            Console.WriteLine($"{empresa.nome} trabalha quantos anos na empresa?");
+            empresa.temptrab = int.Parse(Console.ReadLine());
+
+            if (empresa.filhos>=1 && empresa.filhos<=3)
+            {   
+                empresa.salario +=/* empresa.salario*/+(500 * empresa.filhos);
+
+            }else
+            {
+                empresa.salario +=/* empresa.salario*/+(500 * 3);
+            }
+            if (empresa.temptrab >= 3 && empresa.temptrab < 5)
+            {
+                Console.WriteLine($"Funcionário(a){empresa.nome} recebe {empresa.salario * 1.05} e tem {empresa.filhos} filho(s) e trabalha a {empresa.temptrab} ano(s)");
+
+            }
+            if (empresa.temptrab > 5 && empresa.temptrab < 9)
+            {
+                Console.WriteLine($"Funcionário(a){empresa.nome} recebe {empresa.salario * 1.10} e tem {empresa.filhos} filho(s) e trabalha a {empresa.temptrab} ano(s)");
+
+            }
+            if (empresa.temptrab >= 10)
+            {
+                Console.WriteLine($"Funcionário(a){empresa.nome} recebe {empresa.salario * 1.20} e tem {empresa.filhos} filho(s) e trabalha a {empresa.temptrab} ano(s)");
+            }
+            if(empresa.temptrab<3) {
+                Console.WriteLine($"Funcionário(a){empresa.nome} recebe {empresa.salario} e tem {empresa.filhos} filho(s) e trabalha a {empresa.temptrab} ano(s)");
+            }
+        Console.ReadKey(); 
+        }
+    }
+}
