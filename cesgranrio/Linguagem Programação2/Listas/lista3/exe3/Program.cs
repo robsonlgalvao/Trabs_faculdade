f35abd38037ab8304ce4009e44fe61887c7cdceb
Robson Galvão <68071194+robsonlgalvao@users.noreply.github.com>
2023-10-01 22:51:33 -0300

    continuarexe4lista3

---
+++ cesgranrio/Linguagem Programação2/Listas/lista3/exe3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace exe3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/* 
+            Desenvolva um programa no qual exiba na tela por 10 vezes a data e hora do sistema 
+            em um período de 4 segundos
+            */
+            
+            for (int i = 0;i < 10;i++) {
+                DateTime Atual = DateTime.Now;
+                Console.WriteLine(Atual);
+                Thread.Sleep(4000);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cesgranrio/Linguagem Programação2/Listas/lista3/exe4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/* 
+            Desenvolva um programa com finalidade similar à de uma loteria, no qual o usuário 
+            entrará com seis números em um intervalo de 1 a 60. Então o programa irá “sortear” 
+            as dezenas e informar quantos números o usuário acertou. Caso ele acerte 3 números 
+            informar que ele fez um terno, caso acerte 4 números informar que ele fez uma 
+            quadra, e assim sucessivamente até o limite de 6 números
+         */
+            int[] num = new int[6];
+            Console.WriteLine("Digite 6 números para o sorteio: ");
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine($"Digite o{i+1}º número para sorteio");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
